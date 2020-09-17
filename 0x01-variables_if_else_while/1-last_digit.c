d1bd445a569fe5f8bc665b81822b7775eb86addf
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-09-17 12:36:40 -0300

    update file 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
+	int last_dig;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int last_digit = n % 10;
-	if (last_digit > 5)
+	last_dig = n % 10;
+	if (last_dig > 5)
 	{
-	printf("Last digit of %i is %i and is greater than 5\n", n, last_digit);
+	printf("Last digit of %i is %i and is greater than 5\n", n, last_dig);
 	}
-	else if (last_digit < 6 && last_digit != 0)
+	else if (last_dig < 6 && last_dig != 0)
 	{
-	printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last_digit);
+	printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last_dig);
 	}
 	else
 	{
-	printf("Last digit of %i is %i and is 0\n", n, last_digit);
+	printf("Last digit of %i is %i and is 0\n", n, last_dig);
 	}
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+int main(void)
+{
+        char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if(ch != 'q' || ch != 'e')
+		{
+			putchar(ch);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
