7b12636e108c495a90ca77de951c22bef710f472
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-09-17 12:56:17 -0300

    adding file 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		printf("%i", n);
+	}
+
+	putchar('\n');
+	return (0);
+}
