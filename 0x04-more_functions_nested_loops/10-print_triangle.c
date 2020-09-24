070b3d33f4a305bc7c83e32407a2d52bc94d6414
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-09-24 11:17:39 -0300

    adding file 9 and 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+void print_triangle(int size)
+{
+
+	int x, y, z;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (x = 0; x < size; x++)
+	{
+		for (y = size - x; y > 1; y--)
+			_putchar(' ');
+		for (z = x + y; z >= 1; z--)
+			_putchar('#');
+		_putchar('\n');
+	}
+
+}
