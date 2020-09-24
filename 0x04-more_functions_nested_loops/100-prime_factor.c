7f39229b7e0f97dd6f25d7cd071fb28c632d7e90
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-09-24 15:27:39 -0300

    adding file 100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+/**
+  * main - entry block
+  * @void: no argument
+  * Return: 0
+  */
+int main(void)
+{
+	long int x; 
+	long int num = 612852475143;
+
+	for (x  = 2; x <= num; x++)
+	{
+		if (num % x == 0)
+		{
+			num = num / x;
+			x--;		
+		}
+	}
+	printf("%li\n", x);
+	return (0);
+}
