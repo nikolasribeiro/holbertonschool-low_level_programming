beb4999ed27cdcd85ce3795b9426b2169991a043
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-09-24 13:47:43 -0300

    delete file 5-main.c 5-more_numbers and fix file 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,15 +4,15 @@ void print_line(int n)
 {
 	int x;
 	
-	if( n <= 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for(x = 0; x < n; x++)
+		for (x = 0; x < n; x++)
 		{
-			_putchar(95);
+			_putchar('_');
 		}
 	}
 	_putchar('\n');
