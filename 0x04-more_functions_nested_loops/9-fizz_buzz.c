7f1a8cd76dbe682f01f4eda37ab86150facaebbe
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-09-24 14:29:16 -0300

    fix file 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,35 +1,38 @@
+
 #include <stdio.h>
 /**
-* main - entry block
-* @void: no argument
-* Return: 0 Success
-*/
+ *main - aaaa
+ *Return: lalaalal
+ */
 int main(void)
 {
-	int i;
-
-	for (i = 1; i < 100; i++)
-	{
-		if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else if ((i % 3 == 0) && (i % 5 == 0))
-		{
-			printf("FizzBuzz ");
-		}
-		else
-		{
-			printf("%i ", i);
-		}
-	}
-
-	printf("\n");
-
-
-	return (0);
+int a;
+for (a = 1; a <= 100; a++)
+{
+if (a == 100)
+{
+printf("Buzz");
+}
+else
+{
+if (((a % 3) == 0) && ((a % 5) == 0))
+{
+printf("FizzBuzz ");
+}
+else if ((a % 3) == 0)
+{
+printf("Fizz ");
+}
+else if ((a % 5) == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%i ", a);
+}
+}
+}
+putchar('\n');
+return (0);
 }
