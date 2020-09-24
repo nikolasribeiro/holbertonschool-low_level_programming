cc131eaf17696087b024a6bd6b026d324e4656bb
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-09-24 14:32:54 -0300

    fix file 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,33 +6,34 @@
  */
 int main(void)
 {
-int a;
-for (a = 1; a <= 100; a++)
-{
-if (a == 100)
-{
-printf("Buzz");
-}
-else
-{
-if (((a % 3) == 0) && ((a % 5) == 0))
-{
-printf("FizzBuzz ");
-}
-else if ((a % 3) == 0)
-{
-printf("Fizz ");
-}
-else if ((a % 5) == 0)
-{
-printf("Buzz ");
-}
-else
-{
-printf("%i ", a);
-}
-}
-}
-putchar('\n');
-return (0);
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a == 100)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			if (((a % 3) == 0) && ((a % 5) == 0))
+			{
+				printf("FizzBuzz ");
+			}
+			else if ((a % 3) == 0)
+			{
+				printf("Fizz ");
+			}
+			else if ((a % 5) == 0)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("%i ", a);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
