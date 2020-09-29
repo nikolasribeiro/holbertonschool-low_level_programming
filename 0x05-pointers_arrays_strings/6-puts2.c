0c2135ef6fd5d43d292b5d3c3b27839ae004af55
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-09-29 11:25:22 -0300

    edit file 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,16 @@
 #include "holberton.h"
-
+/**
+* puts2 - prints a string with two
+* @str: takes in an char array
+* Return: none
+*/
 void puts2(char *str)
 {
 	int count;
 
-	for(count = 0; str[count] != '\0'; count++)
+	for (count = 0; str[count] != '\0'; count++)
 	{
-		if(str[count] % 2 == 0)
+		if (str[count] % 2 == 0)
 		{
 			_putchar(str[count]);
 		}
