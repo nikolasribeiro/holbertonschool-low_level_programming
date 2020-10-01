3ed9590b23a76994dae5e2736e61cb7ae91724fe
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-10-01 15:03:14 -0300

    adding file 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+* leet - encodes a string to 1337
+* @s: takes in a string
+* Return: s, the string
+*/
+char *leet(char *s)
+{
+	int i, j;
+
+	char letters[] = "aeotlAEOTL";
+	char numbers[] = "4307143071";
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = numbers[j];
+			}
+		}
+	}
+
+	return (s);
+}
