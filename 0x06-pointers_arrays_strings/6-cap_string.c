496cc6e0b7540476378df5ea5670b7f8ecc7382c
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-10-01 13:56:12 -0300

    check commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+/**
+  * cap_string - capitalizes all words of a string
+  * @s: takes in a string
+  * Return: s, the string
+  */
+char *cap_string(char *s)
+{
+	int i;
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+	{
+		s[0] -= 32;
+	}
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	
+		if(i==0)
+		{
+			if(s[i]>='a' && s[i]<='z')
+			{
+				s[i]=s[i]-32;
+			}
+			continue;
+		}
+		if(s[i]==' ')
+		{
+			++i;
+			if(s[i]>='a' && s[i]<='z')
+			{
+				s[i]=s[i]-32;
+				continue;
+			}
+		}
+		else
+		{
+			if(s[i]>='A' && s[i]<='Z')
+			{
+				s[i]=s[i]+32;
+			}
+		}
+	}
+	return (s);
+}
