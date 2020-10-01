acfe0c2fd94101e09727d91da51c2c6148af3a61
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-10-01 15:52:59 -0300

    fix file 8

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -8,20 +8,22 @@
  */
 char *rot13(char *a)
 {
-int i = 0, j = 0;
-char letra1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-char letra2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-while (*(a + i) != 0)
-{
-for (j = 0; j <= 52; j++)
-{
-if (*(a + i) == letra1[j])
-{
-*(a + i) = letra2[j];
-break;
-}
-}
-i++;
-}
-return (a);
+	int i = 0, j = 0;
+	char letra1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char letra2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	
+	while (*(a + i) != 0)
+	{
+		for (j = 0; j <= 52; j++)
+		{
+			if (*(a + i) == letra1[j])
+			{
+				*(a + i) = letra2[j];
+				break;
+			}
+		}
+		i++;
+	}
+
+	return (a);
 }
