650059939e350444003d1373b129319bc5d91c0c
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-10-01 12:42:36 -0300

    edit file 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
-
+/**
+* _strncat - concats two strings
+* @dest: destination of new string
+* @src: source of string
+* @n: amount of characters you want to print from src
+* Return: return dest
+*/
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0, j;
