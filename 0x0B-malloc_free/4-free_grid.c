92778c9fa5aa6f862357d8d95857bda35bcbeeba
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-10-14 14:12:03 -0300

    adding file 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+  * free_grid - free grid space
+  * @grid: takes in the grid from #3
+  * @height: takes in the height
+  */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || grid == 0)
+	{
+		return;
+	}
+	else
+	{
+		for (i = 0; i < height; i++)
+		{
+			free(grid[i]);
+		}
+		free(grid);
+	}
+}
