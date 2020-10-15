ab661945beffefee98066d6c2e57e0f6e18f6295
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-10-15 13:52:12 -0300

    fix file 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,33 +1,47 @@
+
 #include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
+
 /**
-  * _calloc - function that allocates memory from an array using malloc
-  * @nmemb: number of elements to be allocated
-  * @size: size of the elements
-  * Return: pointer to the allocated memory of NUll if failed
-  */
+ * _memset - prints buffer in hexa
+ * @s: buffer
+ * @b: write
+ * @n: size
+ * Return: Nothing.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
+/**
+ * _calloc - function that concatenates two strings.
+ * @nmemb: string s1.
+ * @size: string s2.
+ *
+ * Return: Always 0.
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *neo;
-	unsigned int i;
+	void *str;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
 
-	neo = malloc(nmemb * size);
-
-	if (neo == NULL)
+	str = malloc(nmemb * size);
+	if (str == NULL)
 	{
 		return (NULL);
 	}
+	_memset(str, 0, nmemb * size);
 
-
-	for (i = 0; i < nmemb * size; i++)
-	{
-		neo[i] = 0;
-	}
-
-	return (neo);
+	return (str);
 }
