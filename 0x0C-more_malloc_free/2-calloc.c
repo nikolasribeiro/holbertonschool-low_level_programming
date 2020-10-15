1c86f36140bb2ff3261881751c7d00457d0cba10
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-10-15 13:35:19 -0300

    adding file 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+  * _calloc - function that allocates memory from an array using malloc
+  * @nmemb: number of elements to be allocated
+  * @size: size of the elements
+  * Return: pointer to the allocated memory of NUll if failed
+  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *neo;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	neo = malloc(nmemb * size);
+	for (i = 0; i < size; i++)
+	{
+		neo[i] = 0;
+	}
+	return (neo);
+}
