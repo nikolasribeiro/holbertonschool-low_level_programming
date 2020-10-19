781b1ad408ad8bc7e0a2cdb85b7df01d805f74ab
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-10-19 13:33:31 -0300

    adding comments to dog.h

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,10 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "dog.h"
-
+/**
+* free_dog - function that frees dogs
+* @d: dog pointer
+*
+*/
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
+	if (d != NULL)
 	{
 		free(d->name);
 		free(d->owner);
