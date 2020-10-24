8b2fe4686e3f3868026e4cb6d09e3296e04ba59c
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-10-24 13:20:50 -0300

    fix file 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,16 +6,10 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i, sum;
+	unsigned int i, sum = 0;
 	va_list valist;
 
-	if (n == 0)
-	{
-		return (0);
-	}
-
 	va_start(valist, n);
-	sum = 0;
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(valist, int);
