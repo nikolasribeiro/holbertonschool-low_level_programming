8f9ba58f40a6c75a21842e0fb377db7d162b74e5
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-10-29 13:09:20 -0300

    fix file 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,13 +5,13 @@
 */
 void free_list(list_t *head)
 {
-	list_t *mr_robot;
-
 	while (head != NULL)
 	{
-		mr_robot = head;
-		head = mr_robot->next;
-		free(mr_robot->str);
-		free(mr_robot);
+		free(head->next);
+		if (head->str)
+		{
+			free(head->str);
+		}
+		free(head);
 	}
 }
