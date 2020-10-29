621bd2c6bc2f74756323c1ecc7b7cad2bebec00c
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-10-29 13:07:08 -0300

    fix file 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -18,8 +18,10 @@ size_t print_list(const list_t *h)
 		{
 			printf("[0] (nil)\n");
 		}
-
-		printf("[%d] %s\n", h->len, h->str);
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
 		counter++;
 		h = h->next;
 	}
