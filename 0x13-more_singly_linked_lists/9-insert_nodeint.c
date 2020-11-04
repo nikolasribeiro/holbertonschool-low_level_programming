efac23999f0454baa8af07fe703dc888e79d820e
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-11-04 10:28:45 -0300

    edit file 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,7 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	insert->n = n;
 
-	if (idx == 0)
+	if (idx == 0 && *head != NULL)
 	{
 		insert->next = *head;
 		*head = insert;
