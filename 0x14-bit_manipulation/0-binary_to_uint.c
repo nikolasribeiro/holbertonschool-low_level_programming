3a0413bdb8777489f1b5a1259f1d183587e2e00d
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-11-05 15:53:59 -0300

    adding project finished about bit_manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+* binary_to_uint - function that converts binary to unsigned int
+* @b: takes in a conts char string
+* Return: the converted unsigned int
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int counter = 0;
+	unsigned int decimal = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	while (b[counter] != '\0')
+	{
+		if (b[counter] != '0' && b[counter] != '1')
+			return (0);
+		decimal <<= 1;
+		if (b[counter] == '1')
+			decimal ^= 1;
+		counter++;
+	}
+	return (decimal);
+}
