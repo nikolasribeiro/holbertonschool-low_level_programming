5e040e4a348cffac4fdd64cff02de51bb3066720
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-11-05 16:00:07 -0300

    adding file 100

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* get_endianness - checks the endianness
+* @void: no argument
+* Return: 1 if big endian and 0 if little
+*/
+int get_endianness(void)
+{
+	unsigned int i = 1;
+
+	char *n = (char *)&i;
+
+	if (*n)
+		return (1);
+	return (0);
+}
