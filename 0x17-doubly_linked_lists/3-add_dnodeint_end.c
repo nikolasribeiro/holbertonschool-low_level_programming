3978ad09992ba6b53e4270edf4aed939a1d5a260
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2020-12-13 15:16:23 -0300

    fix file 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,8 +10,7 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-	dlistint_t *last_node = *head;
+	dlistint_t *new_node, *last_node;
 
 	new_node = malloc(sizeof(dlistint_t));
 
