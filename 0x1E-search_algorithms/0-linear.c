e91344cd3610cce991d57999a0d70808f9c1bc62
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2021-04-20 17:27:01 -0300

    fix files

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,24 +1,22 @@
 #include "search_algos.h"
-/**
- * linear_search - searches for a value in an array of integers
- * @array: pointer to the first element of the array
- * @size: number of elements in array
- * @value: value to search
- *
- * Return: first index where value is located
- */
 
+/**
+ * linear_search - function that searches for a value in an array
+ * of integers using the Linear search algorithm
+ * @array: input array elements
+ * @size: the size of array
+ * @value: is the value that algorithm to search
+ * Return: Always EXIT_SUCCESS
+ */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
+	int i;
 
-	if (array == NULL)
-		return (-1);
-	for (i = 0; i < size; i++)
+	for (i = 0; i < (int)size; i++)
 	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
+		printf("Value checked array[%i] = [%i]\n", i, array[i]);
+			if (value == array[i])
+				return (i);
 	}
 	return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,56 +1,40 @@
 #include "search_algos.h"
+
 /**
- * binary_search - searches for a value in an array of integers
- * @array: pointer to the first element of the array
- * @size: number of elements in array
- * @value: value to search
- *
- * Return: first index where value is located
+ * binary_search - function that searches for a value in an array
+ * of integers using the Binary search algorithm
+ * @array: input array elements
+ * @size: the size of array
+ * @value: is the value that algorithm to search
+ * Return: Always EXIT_SUCCESS
  */
 int binary_search(int *array, size_t size, int value)
 {
-	int index = 0;
+	int i;
+	int j;
+	int left = 0;
+	int right = size - 1;
 
-	if (!array)
+	if (array == NULL)
 		return (-1);
-	index = forUchecker(array, value, 0, size - 1, size);
-	return (index);
-}
 
-/**
- * forUchecker - searches for a value in an array of integers
- * @array: pointer to the first element of the array
- * @size: number of elements in array
- * @value: value to search
- * @low: lowest value of the subarray
- * @high: highest value of the subarray
- *
- * Return: first index where value is located
- */
-int forUchecker(int *array, int value, int low, int high, size_t size)
-{
-	int mid = low + (high - low) / 2;
-	int i = 0;
-
-	if (high >= low)
+	while (left <= right)
 	{
 		printf("Searching in array: ");
-		for (i = low; i <= high; i++)
+		for (i = left; i <= right; i++)
 		{
-			if (i == high)
-				printf("%d", array[i]);
+			if (i == right)
+				printf("%i\n", array[i]);
 			else
-				printf("%d, ", array[i]);
+				printf("%i, ", array[i]);
 		}
-		printf("\n");
-		/* If found at mid, then return it */
-		if (array[mid] == value)
-			return (mid);
-		/* Search the left half */
-		if (array[mid] > value)
-			return (forUchecker(array, value, low, mid - 1, size));
-		/* Search the right half */
-		return (forUchecker(array, value, mid + 1, high, size));
+		j = left + (right - left) / 2;
+		if (array[j] == value)
+			return (j);
+		else if (value > array[j])
+			left = j + 1;
+		else if (value < array[j])
+			right = j - 1;
 	}
 	return (-1);
 }
